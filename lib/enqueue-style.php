1ec7293648092ed949a0a6abde6572c239d6a8f1
George Spake <gpspake@gmail.com>
2014-08-13 09:58:23 -0500

    Fix stylesheet path for child-theme compatibility
    use get_stylesheet_directory_uri() instead of get_template_uri() so that proper stylesheet is loaded if childtheme is used.
    Also changed reverie to grunterie :)

--- lib/enqueue-style.php
+++ lib/enqueue-style.php
@@ -3,19 +3,18 @@
 Enqueue the proper CSS
 if you use Sass.
 *********************/
-if( ! function_exists( 'reverie_enqueue_style' ) ) {
-	function reverie_enqueue_style()
+if( ! function_exists( 'grunterie_enqueue_style' ) ) {
+	function grunterie_enqueue_style()
 	{
 		// foundation stylesheet
-		wp_register_style( 'reverie-foundation-stylesheet', get_template_directory_uri() . '/css/app.css', array(), '' );
+		wp_register_style( 'grunterie-foundation-stylesheet', get_stylesheet_directory_uri() . '/css/app.css', array(), '' );
 
 		// Register the main style
-		wp_register_style( 'reverie-stylesheet', get_template_directory_uri() . '/css/style.css', array(), '', 'all' );
+		wp_register_style( 'grunterie-stylesheet', get_stylesheet_directory_uri() . '/css/style.css', array(), '', 'all' );
 		
-		wp_enqueue_style( 'reverie-foundation-stylesheet' );
-		wp_enqueue_style( 'reverie-stylesheet' );
+		wp_enqueue_style( 'grunterie-foundation-stylesheet' );
+		wp_enqueue_style( 'grunterie-stylesheet' );
 		
 	}
 }
-add_action( 'wp_enqueue_scripts', 'reverie_enqueue_style' );
-?>
+add_action( 'wp_enqueue_scripts', 'grunterie_enqueue_style' );
