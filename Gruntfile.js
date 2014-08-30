092f402668ecf5121ff2fc663c984d6fb3fafdc0
GeorgeSpake <gpspake@gmail.com>
2014-08-30 18:29:56 -0500

    Better Grunt
    Refactor libsass vs compass options
    Separate build task from default
    Update compass task to get rid of config.rb

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,94 +1,80 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    sass: {
-      options: {
-        includePaths: ['bower_components/foundation/scss']
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed'
+        sass: {
+            options: {
+                includePaths: ['bower_components/foundation/scss']
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'css/app.css': 'scss/app.scss',
+                    'css/style.css': 'scss/style.scss'
+                }
+            }
         },
-        files: {
-          'css/app.css': 'scss/app.scss',
-          'css/style.css': 'scss/style.scss'
-        }        
-      }
-    },
+
+        compass: {
+            dist: {
+                options: {
+                    importPath: 'bower_components/foundation/scss',
+                    sassDir: 'scss',
+                    cssDir: 'css',
+                    environment: 'production',
+                    outputStyle: 'compressed'
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'js/modernizr.min.js': ['bower_components/modernizr/modernizr.js']
+                }
+            }
+        },
+
+        copy: {
+            main: {
+                nonull: true,
+                src: 'bower_components/foundation/js/foundation.min.js',
+                dest: 'js/foundation.min.js'
+            }
+        },
+
+        watch: {
+            grunt: { files: ['Gruntfile.js'] },
+
+            sass: {
+                files: 'scss/**/*.scss',
+                tasks: ['sass']
+            },
+
+            compass: {
+                files: 'scss/**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-compass')
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     /*
-     * Uncomment this section to use compass
+     * To use Compass instead of lib-sass, uncomment this line and comment out the one below it:
      */
-    //compass: {                  // Task
-    //dist: {                   // Target
-    //      options: {              // Target options
-    //          sassDir: 'scss',
-    //          cssDir: 'css'
-    //      }
-    //  }
-    //},
+    grunt.registerTask('style', ['sass']);
+    //grunt.registerTask('style', ['compass']);
 
-    uglify: {
-    options: {
-      mangle: false
-    },
-    my_target: {
-      files: {
-        'js/modernizr.min.js': ['bower_components/modernizr/modernizr.js']
-      }
-    }
-    },
-
-    copy: {
-      main: {
-        nonull: true,
-        src: 'bower_components/foundation/js/foundation.min.js',
-        dest: 'js/foundation.min.js'
-      }
-    },
-
-    watch: {
-      grunt: { files: ['Gruntfile.js'] },
-
-      /*
-       *To use Compass instead of lib-sass, comment out or delete this section and uncomment the one under it:
-       */
-      sass: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass']
-      }
-
-      /*
-       * Uncomment to use compass:
-       */
-
-      //compass: {
-      //    files: 'scss/**/*.scss',
-      //    tasks: ['sass']
-      //}
-
-    }
-  });
-
-  /*
-   * To use Compass instead of lib-sass, uncomment thiis line:
-   */
-  //grunt.loadNpmTasks('grunt-contrib-compass')
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-
-  /*
-   * To use Compass instead of lib-sass, comment out or delete this line and uncomment the one under it:
-   */
-  grunt.registerTask('build', ['sass','uglify','copy']);
-
-  /*
-   * Uncomment to use compass:
-   */
-  //grunt.registerTask('build', ['compass','uglify','copy']);
-
-  grunt.registerTask('default', ['build','watch']);
+    grunt.registerTask('build', ['style','uglify','copy']);
+    grunt.registerTask('default', ['style','watch']);
 }
